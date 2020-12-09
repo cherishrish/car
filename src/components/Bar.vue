02e6bce3d735caa79679293024ea1b08895cef01
feifei <feifei@car.com>
2020-12-09 20:58:45 +0800

    环状饼图功能基本实现

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="bar">
-    <div id="bar1"></div>
-    <div id="bar2"></div>
+    <div id = "d1">
+      <img id="back1" src="../../static/back.png" @click="goBack()">
+      <div id="bar1"></div>
+    </div>
+    <div id="d2">
+      <div id="bar2"></div>
+    </div>
   </div>
 </template>
 
@@ -14,6 +19,9 @@
                     data: []
                 },
                 myChart: '',
+                back1:'',
+                proName:'',
+                cityName:'',
                 index:0,
                 name:'',
                 proId:1,
@@ -23,9 +31,9 @@
 
         mounted() {
             this.myChart = this.$echarts.init(document.getElementById('bar1'));
-            this.init()
+            this.init();
             this.myChart.on('click',  (param)=> {
-                console.log(this.index)
+                console.log(this.index);
                 if(this.index===0){
                     this.name = param.name;
                     this.drawPie(1);
@@ -33,6 +41,7 @@
                     this.$axios.get('/static/province.json').then(res => {
                         for (var i = 0; i < res.data.length; i++) {
                             if(param.name===res.data[i].proname){
+                                this.proName = param.name;
                                 this.proId = parseInt(res.data[i].proid);
                                 break;
                             }
@@ -45,6 +54,7 @@
                     this.$axios.get('/static/city.json').then(res => {
                         for (var i = 0; i < res.data.length; i++) {
                             if(param.name===res.data[i].cityName){
+                                this.cityName = param.name;
                                 this.city = parseInt(res.data[i].cityID);
                                 break;
                             }
@@ -75,6 +85,16 @@
                 })
             },
 
+            goBack(){
+                console.log(this.index);
+                if(this.index>0){
+                    this.index--;
+                    console.log(this.index);
+                    this.drawPie(this.index,this.proId,this.city);
+                    this.index--;
+                }
+            },
+
             drawBar() {
                 this.myChart.setOption({
                     tooltip: {//提示框，可以在全局也可以在
@@ -85,6 +105,10 @@
                             color: "black",
                         }
                     },
+                    title:{
+                        text:(this.index>0?this.name:'')+(this.index>1?this.proName:'全国')+(this.index>2&&this.proName!==this.cityName?this.cityName:'')+'销量分布图',
+                        left:'center'
+                    },
                     series: [
                         {
                             name: '访问来源',
@@ -92,7 +116,23 @@
                             radius: ['40%', '60%'],//饼图的半径，第一个为内半径，第二个为外半径
                             // color: ['#D1FBEF', '#F9D858', '#4CD0DD', '#DF86F0', '#F5A7C1'],
                             label: {
-                                formatter: '{a|{b}\n{d}%}',
+                                // formatter: '{a|{b}\n{d}%}',
+                                formatter(v) {
+                                    let text = v.name+Math.round(v.percent)+'%'
+                                    if(text.length <= 6)
+                                    {
+                                        return text;
+                                    }else if(text.length > 6 && text.length <= 12){
+                                        return text = `${text.slice(0,6)}\n${text.slice(6)}`
+                                    }else if(text.length > 12 && text.length <= 18){
+                                        return text = `${text.slice(0,6)}\n${text.slice(6,12)}\n${text.slice(12)}`
+                                    }else if(text.length > 18 && text.length <= 24){
+                                        return text = `${text.slice(0,6)}\n${text.slice(6,12)}\n${text.slice(12,18)}\n${text.slice(18)}`
+                                    }else if(text.length > 24){
+                                        return text = `${text.slice(0,6)}\n${text.slice(6,12)}\n${text.slice(12,18)}\n${text.slice(18,24)}\n${text.slice(24)}`
+                                    }
+                                },
+                                align:'left',
                                 borderWidth: 1,
                                 borderRadius: 4,
                                 color: '#D1FBEF',
@@ -114,7 +154,22 @@
             },
 
             drawPie(index,proId,city){
-                if(index===1){
+                if(index===0){
+                    this.$axios.get('/static/car.json').then(res => {
+                        this.objectData.data=[];
+                        for (var i = 0; i < res.data.length; i++) {
+                            var map = {
+                                "name": "",
+                                "value": 1
+                            };
+                            map.name = res.data[i].type;
+                            map.value = res.data[i].sale;
+                            this.objectData.data[i] = map;
+                        }
+                        console.log(this.objectData.data);
+                        this.drawBar()
+                    })
+                }else if(index===1){
                     this.$axios.get('/static/province.json').then(res => {
                         this.objectData.data=[];
                         for (var i = 0; i < res.data.length; i++) {
@@ -138,7 +193,7 @@
                                     "value": 1
                                 };
                                 map.name = res.data[i].cityName;
-                                map.value = parseInt(Math.random()*(200-100+1)+1000,10);
+                                map.value = parseInt(Math.random()*(200-100+1)+100,10);
                                 this.objectData.data.push(map)
                             }
                         }
@@ -160,7 +215,7 @@
                                         "value": 1
                                     };
                                     map.name = res.data[i].company;
-                                    map.value = parseInt(Math.random()*(20-4+1)+1000,10);
+                                    map.value = parseInt(Math.random()*(20-4+1)+4,10);
                                     this.objectData.data.push(map)
                                 }else {
                                     prev = curr;
@@ -191,14 +246,27 @@
 
   }
 
-  .bar #bar1 {
+  .bar #d1 {
     position: absolute;
     width: 100%;
     height: 50%;
     background: #003da8;
   }
 
-  .bar #bar2 {
+  .bar #back1{
+    position: absolute;
+    right:5%;
+  }
+
+  .bar #bar1{
+    position: absolute;
+    width: 100%;
+    top:9%;
+    height: 100%;
+    background: #003da8;
+  }
+
+  .bar #d2 {
     position: absolute;
     width: 100%;
     top: 50%;
